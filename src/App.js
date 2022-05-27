ad2322abee91c1bf79336057733048ae1629d047
CJ-Codes <ccoreas484@gmail.com>
2022-05-26 23:02:10 -0400

    basic html layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import bean from "./Images/beanofpatch.png";
+import buy3 from "./Images/buy3get1.jpg";
+import music from "./Images/liveMusic.jpeg";
+import mic from "./Images/OpenMic.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft } from "@fortawesome/free-solid-svg-icons"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Menu</li>
+          <img src={bean} alt='coffee'/>
+          <li>Locations</li>
+          <li>About Us</li>
+        </ul>
+      </nav>
+      <div className='firstSection'>
+        <div className='mySlides fade'>
+          <div className='numberText'>1 / 3</div>
+          <img src={buy3} alt='buy3get1'/>
+          <h3 className='text'>Nobody likes monday's so we made it better</h3>
+        </div>
+        <div className='mySlides fade'>
+          <div className='numberText'>2 / 3</div>
+          <img src={music} alt='liveMusic'/>
+          <h3 className='text'>Sit back and relax to the music</h3>
+        </div>
+        <div className='mySlides fade'>
+          <div className='numberText'>3 / 3</div>
+          <img src={mic} alt='openMic'/>
+          <h3 className='text'>Show us your talent tonight</h3>
+        </div>
+        <a className='next'><FontAwesomeIcon icon={faChevronRight}/></a>
+        <a className='prev'><FontAwesomeIcon icon={faChevronLeft}/></a>
+      </div>
+      <br/>
+      <div>
+        <span className='dot'></span>
+        <span className='dot'></span>
+        <span className='dot'></span>
+      </div>
+
+      <div className='secondSection'>
+        <div className='cubeInfo'>
+          <h5>Local Artist</h5>
+        </div>
+        <div className='cubeInfo'>
+          <h5>Bake Contest</h5>
+        </div>
+        <div className='cubeInfo'>
+          <h5>Private Conference</h5>
+        </div>
+      </div>
+      <div className='thirdSection'>
+        <h2>Bringing the world to you, We bring different variety</h2>
+      </div>
+      <footer>
+        <h5>Copyright @ 2022 | Web Designed by: Carlos Coreas</h5>
+      </footer>
     </div>
   );
 }
