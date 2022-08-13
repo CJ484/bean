3699118dd8a86b5086371bcdf7ecfae5a43ff7bd
CJ-Codes <ccoreas484@gmail.com>
2022-08-13 11:49:17 -0400

    added better pictures and added styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 import './App.css';
 import bean from "./Images/beanofpatch.png";
-import buy3 from "./Images/buy3get1.jpg";
-import music from "./Images/liveMusic.jpeg";
+import buy3 from "./Images/buy2.jpg";
+import music from "./Images/liveMusic.jpg";
 import mic from "./Images/OpenMic.jpg"
+import conferance from "./Images/Conferance.jpg"
+import localArtist from "./Images/anotherOpenMic.jpg"
+import bake from "./Images/bakeContest.jpg"
+import culture from "./Images/worldCoffee.jpg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight, faChevronLeft } from "@fortawesome/free-solid-svg-icons"
+import React, { useEffect } from 'react';
+
+let i;
+let slideIndex = 1;
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex =n);
+}
+
+function showSlides(n) {
+  let slides = document.querySelectorAll(".mySlides");
+  let dots = document.querySelectorAll(".dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+const useIt = () => {
+  useEffect(() => {
+    showSlides(slideIndex)
+  })
+}
 
 function App() {
+  useIt();
   return (
     <div>
       <nav>
@@ -34,28 +78,30 @@ function App() {
           <img src={mic} alt='openMic'/>
           <h3 className='text'>Show us your talent tonight</h3>
         </div>
-        <a className='next'><FontAwesomeIcon icon={faChevronRight}/></a>
-        <a className='prev'><FontAwesomeIcon icon={faChevronLeft}/></a>
+        <a className='prev' onClick={() => plusSlides(-1)}><FontAwesomeIcon icon={faChevronLeft}/></a>
+        <a className='next' onClick={() => plusSlides(1)}><FontAwesomeIcon icon={faChevronRight}/></a>
       </div>
-      <br/>
-      <div>
-        <span className='dot'></span>
-        <span className='dot'></span>
-        <span className='dot'></span>
+      <div className='dots'>
+        <span className='dot' onClick={() => currentSlide(1)}></span>
+        <span className='dot' onClick={() => currentSlide(2)}></span>
+        <span className='dot' onClick={() => currentSlide(3)}></span>
       </div>
-
       <div className='secondSection'>
         <div className='cubeInfo'>
           <h5>Local Artist</h5>
+          <img src={localArtist} alt='local artist singing' />
         </div>
         <div className='cubeInfo'>
+          <img src={bake} alt="Backing contest" />
           <h5>Bake Contest</h5>
         </div>
         <div className='cubeInfo'>
           <h5>Private Conference</h5>
+          <img src={conferance} alt="Studying conferance table" />
         </div>
       </div>
       <div className='thirdSection'>
+        <img src={culture} alt="Diverse coffee from around the world" />
         <h2>Bringing the world to you, We bring different variety</h2>
       </div>
       <footer>
