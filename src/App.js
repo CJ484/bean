87009e5a1b26bbc316b9f493ad495cda02474c4b
CJ-Codes <ccoreas484@gmail.com>
2022-09-24 22:39:49 -0400

    completed the web portion now moving to the mobile

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import conferance from "./Images/Conferance.jpg"
 import localArtist from "./Images/anotherOpenMic.jpg"
 import bake from "./Images/bakeContest.jpg"
 import culture from "./Images/worldCoffee.jpg"
+import wifi from './Images/freeWifi.png';
+import bean2 from './Images/BayShoreBean.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faChevronLeft, fa } from "@fortawesome/free-solid-svg-icons"
+import { faChevronRight, faChevronLeft } from "@fortawesome/free-solid-svg-icons"
 import { faFacebook, faInstagram, faTiktok, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import React, { useEffect } from 'react';
 
@@ -56,10 +58,18 @@ function App() {
     <div>
       <div className="social">
         <h6>Catch us on Social Media:</h6>
-        <a data-hover="Facebook"><FontAwesomeIcon icon={faFacebook} /></a>
-        <a data-hover="Instagram"><FontAwesomeIcon icon={faInstagram}/></a>
-        <a data-hover="Twitter"><FontAwesomeIcon icon={faTwitter}/></a>
-        <a data-hover="TikTok"><FontAwesomeIcon icon={faTiktok}/></a>
+        <a data-hover="Facebook">
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a data-hover="Instagram">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a data-hover="Twitter">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a data-hover="TikTok">
+          <FontAwesomeIcon icon={faTiktok} />
+        </a>
       </div>
       <nav>
         <ul>
@@ -133,9 +143,9 @@ function App() {
             <h5>Private Conference</h5>
             <img src={conferance} alt="Studying conferance table" />
           </div>
-          <h6 className='textBox'>
-            Schedule your next study group or business meeting in our
-            large sitting table. Our stress free enviroment gives a change of scenary
+          <h6 className="textBox">
+            Schedule your next study group or business meeting in our large
+            sitting table. Our stress free enviroment gives a change of scenary
             to help a new perspective.
           </h6>
         </div>
@@ -144,42 +154,42 @@ function App() {
         <img src={culture} alt="Diverse coffee from around the world" />
         <h2>Bringing the world to you, We bring different variety</h2>
       </div>
-      <footer>
-        <div className="top-footer">
-          <img className="bean2" src={bean2}/>
-          <div>
-            <h5>CONTACT INFORMATION</h5>
-            <br/>
-            47 East Main Street
-            <br />
-            Bay Shore, NY 11706
-            <br />
-            P:(631)647-7971
-            <br />
-            E: info@bayshorebean.com
-            <br/>
-            <br/>
-            <h5>OPENING TIMES</h5>
-            <br/>
-            Mon-Tue-Thu 6:00AM - 11:00PM
-            <br />
-            Wed 6:00AM - 11:00PM
-            <br />
-            Fri-Sat 6:00AM - 12:00PM
-            <br />
-            Sun 6:00AM - 8:30PM
-          </div>
-          <div className='map'>
-            <h5>MAP & LOCATION</h5>
-            <br/>
-            <img
-              // src={mic}
-              src="https://maps.googleapis.com/maps/api/staticmap?center=bay+shore+bean&zoom=13&scale=2&size=400x200&maptype=roadmap&format=png&key={API__KEY}&markers=size:mid%7Ccolor:0x8e4e1a%7Clabel:1%7Cbay%20shore%20bean"
-              alt="Google map of bay shore bean"
-            />
-          </div>
-          <img className='wifiPic' src={wifi} />
+      <div className="top-footer">
+        <img className="bean2 topFooterChild" src={bean2} />
+        <div className="topFooterChild">
+          <h5>CONTACT INFORMATION</h5>
+          <br />
+          47 East Main Street
+          <br />
+          Bay Shore, NY 11706
+          <br />
+          P:(631)647-7971
+          <br />
+          E: info@bayshorebean.com
+          <br />
+          <br />
+          <h5>OPENING TIMES</h5>
+          <br />
+          Mon-Tue-Thu 6:00AM - 11:00PM
+          <br />
+          Wed 6:00AM - 11:00PM
+          <br />
+          Fri-Sat 6:00AM - 12:00PM
+          <br />
+          Sun 6:00AM - 8:30PM
         </div>
+        <div className="map topFooterChild">
+          <h5>MAP & LOCATION</h5>
+          <br />
+          <img
+            src={mic}
+            // src="https://maps.googleapis.com/maps/api/staticmap?center=bay+shore+bean&zoom=13&scale=2&size=400x200&maptype=roadmap&format=png&key={API__KEY}&markers=size:mid%7Ccolor:0x8e4e1a%7Clabel:1%7Cbay%20shore%20bean"
+            alt="Google map of bay shore bean"
+          />
+        </div>
+        <img className="wifiPic topFooterChild" src={wifi} />
+      </div>
+      <footer>
         <h5>Copyright @ 2022 | Web Designed by: Carlos Coreas</h5>
       </footer>
     </div>
